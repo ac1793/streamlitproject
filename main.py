032947dd47aa5cdc9a934965cc653723ae87cdc7
ac1793 <129906920+ac1793@users.noreply.github.com>
2023-06-03 19:04:15 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    st.write('reading file as bytes')
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+    st.write('Converting to a string based IO')
 
     # To read file as string:
     string_data = stringio.read()
-    st.write("It's been read")
+    st.write("reading file as string")
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
